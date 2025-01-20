7b08355817dbdd3e43a2a0947885c154391d9fcb
Mac-An-tSaoir <77882851+Mac-An-tSaoir@users.noreply.github.com>
2025-01-20 15:24:00 +0000

    v78_apply watermark,

--- src/main/java/com/plantplaces/photos/plantplacespostprocessor/PhotoProcessor.java
+++ src/main/java/com/plantplaces/photos/plantplacespostprocessor/PhotoProcessor.java
@@ -1,14 +1,18 @@
 package com.plantplaces.photos.plantplacespostprocessor;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.imageio.ImageIO;
+
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
 
 @Component
 public class PhotoProcessor {
@@ -17,11 +21,15 @@ public class PhotoProcessor {
 	public void processPhoto(String path) {
 		System.out.println("Path is: "+path);
 		File file = new File(path);
-		Path thumbNailPath = Paths.get(path).getParent();
+		Path photoPath = Paths.get(path).getParent();
 		Path fileName = Paths.get(path).getFileName();
-		String thumbNailFullPath = thumbNailPath + File.separator + "thumbnail" + File.separator + fileName;
+		String thumbNailFullPath = photoPath + File.separator + "thumbnail" + File.separator + fileName;
 		File thumbNailFile = new File(thumbNailFullPath);
+		String watermarkPath = photoPath + File.separator + "watermark2.png";
+		File watermarkFile = new File(watermarkPath);
 		try {
+			BufferedImage watermark = ImageIO.read(watermarkFile);
+			Thumbnails.of(file).scale(1).watermark(Positions.BOTTOM_CENTER, watermark, 0.9f).toFile(file);
 			Thumbnails.of(file).size(100,100).toFile(thumbNailFile);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
